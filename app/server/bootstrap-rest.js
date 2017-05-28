5b96317264499b8a5756a83a52967e7ce7fbda83
Chris Bates-Keegan <chris@evansfinch.co.uk>
2017-05-28 17:57:34 +0100

    - REFACTORED: Run with clusters for performance. - REFACTORED: Simplified app.js. Bootstrap updated.

--- app/server/bootstrap-rest.js
+++ app/server/bootstrap-rest.js
@@ -12,6 +12,13 @@
 
 const path = require('path');
 const fs = require('fs');
+const os = require('os');
+const cluster = require('cluster');
+const express = require('express');
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
 const Config = require('./config');
 const Model = require('./model');
 const Routes = require('./routes');
@@ -20,10 +27,35 @@ const MongoClient = require('mongodb').MongoClient;
 
 /* ********************************************************************************
  *
+ *
+ *
+ **********************************************************************************/
+const processes = os.cpus().length;
+const _workers = [];
+
+/* ********************************************************************************
+ *
+ * WORKERS
+ *
+ **********************************************************************************/
+const __spawnWorkers = () => {
+  Logging.log(`Spawning ${processes} REST Workers`);
+
+  const __spawn = idx => {
+    _workers[idx] = cluster.fork();
+  };
+
+  for (let x = 0; x < processes; x++) {
+    __spawn(x);
+  }
+};
+
+/* ********************************************************************************
+ *
  * INSTALL
  *
  **********************************************************************************/
-const __systemInit = () => {
+const __systemInstall = () => {
   return Model.Organisation.find({})
     .then(orgs => {
       if (orgs.length > 0) {
@@ -74,8 +106,8 @@ const __systemInit = () => {
       Logging.log('APP ADDED', Logging.Constants.LogLevel.DEBUG);
       Logging.log(res.app.id, Logging.Constants.LogLevel.DEBUG);
       return new Promise((resolve, reject) => {
-        var pathName = path.join(Config.appDataPath, 'super.json');
-        var app = Object.assign(res.app.details, {token: res.token.value});
+        let pathName = path.join(Config.appDataPath, 'super.json');
+        let app = Object.assign(res.app.details, {token: res.token.value});
         fs.writeFile(pathName, JSON.stringify(app), err => {
           if (err) {
             return reject(err);
@@ -96,7 +128,8 @@ const __systemInit = () => {
  **********************************************************************************/
 const __nativeMongoConnect = app => {
   return new Promise((resolve, reject) => {
-    MongoClient.connect(app.get('db-uri'), (err, db) => {
+    const mongoUrl = `mongodb://${Config.mongoDb.url}/${Config.app.code}-${Config.env}`;
+    MongoClient.connect(mongoUrl, (err, db) => {
       if (err) throw err;
       Model.init(db);
       resolve();
@@ -106,22 +139,64 @@ const __nativeMongoConnect = app => {
 
 /* ********************************************************************************
  *
- * RHIZOME REST
+ * WORKER
  *
  **********************************************************************************/
-const _initRestApp = (app, io) => {
-  Logging.setLogApp('rest');
+const __initWorker = () => {
+  let app = express();
+  app.use(morgan('short'));
+  app.enable('trust proxy', 1);
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(methodOverride());
+  app.use(express.static(`${Config.appDataPath}/public`));
 
-  return __nativeMongoConnect(app)
+  return __nativeMongoConnect()
     .then(() => {
       let tasks = [
-        Routes.init(app, io),
-        __systemInit()
+        Routes.init(app),
+        __systemInstall()
       ];
+
+      app.listen(Config.listenPort);
 
       return Promise.all(tasks);
     })
     .catch(Logging.Promise.logError());
+};
+
+/* ********************************************************************************
+ *
+ * MASTER
+ *
+ **********************************************************************************/
+const __initMaster = () => {
+  const isPrimary = Config.rest.app === 'primary';
+  if (isPrimary) {
+    Logging.logDebug(`Primary Master REST`);
+  }
+
+  __spawnWorkers();
+
+  return Promise.resolve();
+};
+
+/* ********************************************************************************
+ *
+ * RHIZOME REST
+ *
+ **********************************************************************************/
+const _initRestApp = () => {
+  Logging.setLogApp('rest');
+
+  let p = null;
+  if (cluster.isMaster) {
+    p = __initMaster();
+  } else {
+    p = __initWorker();
+  }
+
+  return p.then(() => cluster.isMaster);
 };
 
 /* ********************************************************************************
